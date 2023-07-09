2311d3b6952acca1610d818f172bbb72074f7af6
Saoa35 <saoamah@gmail.com>
2023-07-09 14:53:59 +0400

    created userModel

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Chat App");
+});
+
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
 
---
+++ server/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, minlength: 3, maxlength: 20 },
+    email: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+      unique: true,
+    },
+    password: { type: String, required: true, minlength: 8, maxlength: 1500 },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
