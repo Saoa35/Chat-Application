a46ab9e606d37b4e46de446cabe707014735bd80
Saoa35 <saoamah@gmail.com>
2023-07-10 10:06:43 +0400

    created findUser func.

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userController.js");
+const {
+  registerUser,
+  loginUser,
+  findUser,
+} = require("../controllers/userController.js");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/find/:userId", findUser);
 
 module.exports = router;
