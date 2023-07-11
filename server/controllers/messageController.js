7734cfa039027d0ec44b9b72426b86d134c3bb35
Saoa35 <saoamah@gmail.com>
2023-07-11 14:21:38 +0400

    + getMessages

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -18,4 +18,16 @@ const createMessage = async () => {
   }
 };
 
-module.exports = { createMessage };
+const getMessages = async (req, res) => {
+  const { chatId } = req.params;
+
+  try {
+    const messages = await messageModel.find({ chatId });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error);
+    resizeBy.status(500).json(error);
+  }
+};
+
+module.exports = { createMessage, getMessages };
