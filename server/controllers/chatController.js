79c4395234deeb15bbb797b2413a8e47e6abd8c2
Saoa35 <saoamah@gmail.com>
2023-07-11 11:01:43 +0400

    + createChat

---
+++ server/controllers/chatController.js
@@ -0,0 +1,26 @@
+const chatModel = require("../models/chatModel.js");
+
+const createChat = async (req, res) => {
+  const { firstId, secondId } = req.body;
+
+  try {
+    const chat = await chatModel.findOne({
+      members: { $all: [firstId, secondId] },
+    });
+
+    if (chat) {
+      return res.status(200).json(chat);
+    }
+
+    const newChat = new chatModel({
+      members: [firstId, secondId],
+    });
+
+    const response = await newChat.save();
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
