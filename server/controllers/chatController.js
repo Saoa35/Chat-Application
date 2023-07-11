6759e316b9befd51cd9cac6d2b391b60a23b39eb
Saoa35 <saoamah@gmail.com>
2023-07-11 11:41:52 +0400

    created findChat controller

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -39,3 +39,20 @@ const findUserChats = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+const findChat = async (req, res) => {
+  const { firstId, secondId } = req.params;
+
+  try {
+    const chat = await chatModel.find({
+      members: { $all: [firstId, secondId] },
+    });
+
+    res.status(200).json(chat);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { createChat, findChat, findUserChats };
