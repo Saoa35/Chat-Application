0f00c000eeb939da265b9aaf67cc83d24d3d0ced
Saoa35 <saoamah@gmail.com>
2023-07-11 16:38:21 +0400

    created ChatContextProvider

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,3 +1,9 @@
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
+
 export const Chat = () => {
+  const { userChats, isUserChatsLoading, userChatsError } =
+    useContext(ChatContext);
+
   return <>Chat</>;
 };
